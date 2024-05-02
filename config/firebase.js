f993be6ec972fb1e769868e2869f812c5c7372b4
kartikey321 <kartikeymahawar1234@gmail.com>
2024-05-02 15:27:29 +0530

    initial commit

--- config/firebase.js
+++ config/firebase.js
@@ -3,7 +3,7 @@ import admin from "firebase-admin";
 import serviceAccount from "./cladbee-6554e-firebase-adminsdk-7pk5m-e470bb864d.json";
 
 const firebaseAdmin = admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+  credential: admin.credential.cert(serviceAccount),
   storageBucket: "gs://cladbee-6554e.appspot.com",
 });
 
